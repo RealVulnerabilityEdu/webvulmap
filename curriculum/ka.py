81966db38b27beb61554749486285d7c277719d5
huichen-cs <chen@sci.brooklyn.cuny.edu>
2024-01-22 21:07:44 +0000

    made progress for incompleted ACM/IEEE CS Curriculum KA parser by jsonizing KA

--- curriculum/ka.py
+++ curriculum/ka.py
@@ -1,3 +1,6 @@
+import json
+
+
 class KnowledgeArea:
     def __init__(self, ka, short_ka) -> None:
         self.ka = ka
@@ -6,6 +9,15 @@ class KnowledgeArea:
 
     def add_ku(self, ku):
         self.units.append(ku)
+
+    def jsonize(self, indent=2):
+        return json.dumps(self, default=lambda x: x.__dict__, indent=2)
+        # return {
+        #     'ka': self.ka,
+        #     'short_ka': self.short_ka,
+        #     'units': [ku.jsonize() for ku in self.units]
+        # }
+
 
 class KnowledgeUnit:
     def __init__(self, ku) -> None:
@@ -21,6 +33,13 @@ class KnowledgeUnit:
     def add_outcome(self, outcome):
         self.outcomes.append(outcome)
 
+    def add_topic(self, topic):
+        self.topics.append(topic)
+
+    def get_last_topic(self):
+        return self.topics[-1]
+
+
 class KnowledgeTopic:
     def __init__(self, topic, tier=None) -> None:
         self.topic = topic
@@ -30,10 +49,12 @@ class KnowledgeTopic:
     def add_subtopic(self, subtopic):
         self.subtopics.append(subtopic)
 
+
 class LearningOutcome:
     def __init__(self, outcome, mastery) -> None:
         self.outcome = outcome
         self.mastery = mastery
+
 
 class KnowledgeUnitTier:
     def __init__(self, tier, hours) -> None:
--- curriculum/parse_ka.py
+++ curriculum/parse_ka.py
@@ -1,21 +1,27 @@
 import os
 import re
 from enum import Enum
-from ka import KnowledgeArea, KnowledgeUnit, KnowledgeTopic, LearningOutcome, KnowledgeUnitTier
+from ka import (
+    KnowledgeArea,
+    KnowledgeUnit,
+    KnowledgeTopic,
+    LearningOutcome,
+    KnowledgeUnitTier,
+)
 
 
 # PATTERN_IAS_HEADING = r'^\fInformation Assurance and Security \(IAS\)$'
-PATTERN_IAS_HEADING = r'^\f{0,1}(Software Development Fundamentals) \((SDF)\)$'
-PATTERN_KU_HEADING = r'^SDF/([A-Z].*)$'
-PATTERN_TIER_HOURS = r'\[((\d+)\s+([^\s,\,]+)\s+hour[s]{0,1})(,\s*(\d+)\s+([^\s,\,]+)\s+hour[s]{0,1})*\]|\[(Elective)\]'
-PATTERN_KU_TOPICS_HEADING = r'^Topics:$'
-PATTERN_TOPICS_TIER = r'\[Core-Tier\d+\]|\[Elective\]'
-PATTERN_KU_TOPIC_LINE = r'(?!^Learning outomes:$)^•\s*(\w.*)$'
-PATTERN_KU_SUBTOPIC_LINE = r'(?!^Learning outomes:$)^o\s*(\w.*)$'
-PATTERN_KU_LEARNING_OUTCOMES_HEADING = r'^Learning Outcomes:$'
-PATTERN_KU_PARTIAL_OUTCOMES_LINE = r'^\d+\.\s*\w.*$'
-PATTERN_KU_OUTCOMES_LINE = r'^\d+\.\s*(\w.*)\[(.*)\]$'
-PATTERN_PAGE_NUMBER = r'^\f-\s*\d+\s*-$'
+PATTERN_IAS_HEADING = r"^\f{0,1}(Software Development Fundamentals) \((SDF)\)$"
+PATTERN_KU_HEADING = r"^SDF/([A-Z].*)$"
+PATTERN_TIER_HOURS = r"\[((\d+)\s+([^\s,\,]+)\s+hour[s]{0,1})(,\s*(\d+)\s+([^\s,\,]+)\s+hour[s]{0,1})*\]|\[(Elective)\]"
+PATTERN_KU_TOPICS_HEADING = r"^Topics:$"
+PATTERN_TOPICS_TIER = r"\[Core-Tier\d+\]|\[Elective\]"
+PATTERN_KU_TOPIC_LINE = r"(?!^Learning outomes:$)^•\s*(\w.*)$"
+PATTERN_KU_SUBTOPIC_LINE = r"(?!^Learning outomes:$)^o\s*(\w.*)$"
+PATTERN_KU_LEARNING_OUTCOMES_HEADING = r"^Learning Outcomes:$"
+PATTERN_KU_PARTIAL_OUTCOMES_LINE = r"^\d+\.\s*\w.*$"
+PATTERN_KU_OUTCOMES_LINE = r"^\d+\.\s*(\w.*)\[(.*)\]$"
+PATTERN_PAGE_NUMBER = r"^\f-\s*\d+\s*-$"
 
 pattern_ias_heading = re.compile(PATTERN_IAS_HEADING)
 pattern_ku_heading = re.compile(PATTERN_KU_HEADING)
@@ -29,96 +35,124 @@ pattern_ku_outcomes_line = re.compile(PATTERN_KU_OUTCOMES_LINE)
 pattern_page_number = re.compile(PATTERN_PAGE_NUMBER)
 pattern_ku_subtopic_line = re.compile(PATTERN_KU_SUBTOPIC_LINE)
 
-State = Enum('State',
-             ['IN_KA',
-              'KU_HEADING', 'KU_HOURS', 'KU_TOPICS_HEADING', 'TOPICS_TIER', 'TOPICS_LIST',
-              'LEARNING_OUTCOMES_HEADING'])
+State = Enum(
+    "State",
+    [
+        "IN_KA",
+        "KU_HEADING",
+        "KU_HOURS",
+        "KU_TOPICS_HEADING",
+        "TOPICS_TIER",
+        "TOPICS_LIST",
+        "LEARNING_OUTCOMES_HEADING",
+    ],
+)
+
 
 def is_ku_heading(state, line, f, line_stack):
     if state == State.IN_KA and not pattern_ku_heading.match(line):
         return False
     elif state == State.IN_KA and pattern_ku_heading.match(line):
         line = f.readline()
-        if not line: return False
+        if not line:
+            return False
         line_stack.append(line)
         return True if pattern_tier_hours.match(line) else False
     else:
         return False
-        
+
+
 def get_outcome_line(line, f, line_stack):
     while True:
         next_line = f.readline()
-        if not next_line: return line
-        if pattern_page_number.match(next_line): continue
-        if not pattern_ku_partial_outcomes_line.match(next_line) and not pattern_ku_heading.match(next_line):
+        if not next_line:
+            return line
+        if pattern_page_number.match(next_line):
+            continue
+        if not pattern_ku_partial_outcomes_line.match(
+            next_line
+        ) and not pattern_ku_heading.match(next_line):
             if next_line.strip():
-                line = line.strip() + ' ' + next_line.strip()
+                line = line.strip() + " " + next_line.strip()
         else:
             line_stack.append(next_line)
-            break 
+            break
     return line
+
 
 def parse_outcome_line(line):
     matches = pattern_ku_outcomes_line.match(line)
-    if not matches: return None
-    if len(matches.groups()) != 2: return None
+    if not matches:
+        return None
+    if len(matches.groups()) != 2:
+        return None
     return matches.group(1).strip(), matches.group(2).strip()
+
 
 def parse_ka_title_line(line):
     matches = pattern_ias_heading.match(line)
-    if not matches: return None
-    if len(matches.groups()) != 2: return None
+    if not matches:
+        return None
+    if len(matches.groups()) != 2:
+        return None
     return matches.group(1), matches.group(2)
+
 
 def parse_ku_title_line(line):
     matches = pattern_ku_heading.match(line)
-    if not matches: return None
-    if matches.groups() != 1: return None
+    if not matches:
+        return None
+    if len(matches.groups()) != 1:
+        return None
     return matches.group(1).strip()
+
 
 def parse_ku_tier_line(line):
     matches = pattern_tier_hours.match(line)
-    if not matches: return None
+    if not matches:
+        return None
     m_list = [m for m in matches.groups() if m]
     if len(m_list) % 3 == 0:
         tier_list = []
         for i in range(0, len(m_list), 3):
-            tier_list.append(KnowledgeUnitTier(m_list[i+1], m_list[i+2]))
+            tier_list.append(KnowledgeUnitTier(m_list[i + 2], m_list[i + 1]))
         return tier_list
     elif len(m_list) == 1:
         return KnowledgeUnitTier(m_list[0], None)
     else:
         return None
 
+
 def parse_topic_line(line):
     matches = pattern_ku_topic_line.match(line)
-    if not matches: return None
-    if len(matches.groups()) != 1: return None
+    if not matches:
+        return None
+    if len(matches.groups()) != 1:
+        return None
     return matches.group(1).strip()
 
 
 def parse_subtopic_line(line):
     matches = pattern_ku_subtopic_line.match(line)
-    if not matches: return None
-    if len(matches.groups()) != 1: return None
+    if not matches:
+        return None
+    if len(matches.groups()) != 1:
+        return None
     return matches.group(1).strip()
+
 
 def parse_ka(ka_fn):
     ka = None
-    ku = None
     topics_tier = None
     state = None
     line_stack = []
     topics_list = []
-    with open(ka_fn, mode='rt') as f:
+    with open(ka_fn, mode="rt") as f:
         line_stack.append(f.readline())
-        
+
         while line_stack:
             line = line_stack.pop()
-            
-            if '12. Apply consistent documentation and program style standards that contribute to the readability and' in line:
-                pass
-          
+
             if not state and pattern_ias_heading.match(line):
                 state = State.IN_KA
                 ka_title, ka_short_title = parse_ka_title_line(line)
@@ -137,27 +171,35 @@ def parse_ka(ka_fn):
             elif state == State.KU_TOPICS_HEADING and pattern_topics_tier.match(line):
                 state = State.TOPICS_TIER
                 topics_tier = line.strip()
-            elif state == State.KU_TOPICS_HEADING and not pattern_topics_tier.match(line):
+            elif state == State.KU_TOPICS_HEADING and not pattern_topics_tier.match(
+                line
+            ):
                 state = State.TOPICS_TIER
                 topics_tier = None
                 line_stack.append(line)
-            elif (state == State.KU_TOPICS_HEADING or state == State.TOPICS_TIER) and pattern_ku_topic_line.match(line):
+            elif (
+                state == State.KU_TOPICS_HEADING or state == State.TOPICS_TIER
+            ) and pattern_ku_topic_line.match(line):
                 state = State.TOPICS_LIST
-                topics_list = []
                 line_stack.append(line)
             elif state == State.TOPICS_LIST and pattern_ku_topic_line.match(line):
                 topic = parse_topic_line(line)
-                topics_list.append(KnowledgeTopic(topic, topics_tier))
+                ku.add_topic(KnowledgeTopic(topic, topics_tier))
             elif state == State.TOPICS_LIST and pattern_ku_subtopic_line.match(line):
                 subtopic = parse_subtopic_line(line)
-                topics_list[-1].add_subtopic(subtopic)
+                ku.get_last_topic().add_subtopic(subtopic)
             elif state == State.TOPICS_LIST and pattern_ku_outcomes_heading.match(line):
                 state = State.LEARNING_OUTCOMES_HEADING
-            elif state == State.LEARNING_OUTCOMES_HEADING and pattern_ku_partial_outcomes_line.match(line):
+            elif (
+                state == State.LEARNING_OUTCOMES_HEADING
+                and pattern_ku_partial_outcomes_line.match(line)
+            ):
                 outcome_line = get_outcome_line(line, f, line_stack)
                 outcome, mastery = parse_outcome_line(outcome_line)
                 ku.add_outcome(LearningOutcome(outcome, mastery))
-            elif state == State.LEARNING_OUTCOMES_HEADING and pattern_ku_heading.match(line):
+            elif state == State.LEARNING_OUTCOMES_HEADING and pattern_ku_heading.match(
+                line
+            ):
                 state = State.KU_HEADING
                 ku_title = parse_ku_title_line(line)
                 ku = KnowledgeUnit(ku_title)
@@ -169,12 +211,17 @@ def parse_ka(ka_fn):
 
             if not line_stack:
                 line = f.readline()
-                if line: line_stack.append(line) 
-                
+                if line:
+                    line_stack.append(line)
+
+    return ka
+
 
 def main():
-    ka_fn = os.path.join('webknwlmap', 'curriculum', 'data', 'cs2013_web_final_sdf.txt')
-    parse_ka(ka_fn)
+    ka_fn = os.path.join("webknwlmap", "curriculum", "data", "cs2013_web_final_sdf.txt")
+    ka = parse_ka(ka_fn)
+    print(ka.jsonize())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
